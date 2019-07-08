706347b4f25b1d70f48648566c082492a0f8192d
Team <timonsol13@gmail.com>
2019-07-09 01:33:17 +0300

    Clear game files

--- Flixter/Assets/Scripts/Core/EventManager.cs
+++ Flixter/Assets/Scripts/Core/EventManager.cs
@@ -1,12 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using TObject.Shared;
 
 public class EventManager {
-	public static event EventController.MethodContainer OnLocalizationLoadedEvent;
-	public void CallOnLocalizationLoadedEvent(EventData ob = null) => OnLocalizationLoadedEvent?.Invoke(ob);
-
 	public static event EventController.MethodContainer OnTimeStopChangedEvent;
 	public void CallOnTimeStopChangedEvent(EventData ob = null) => OnTimeStopChangedEvent?.Invoke(ob);
 
--- Flixter/Assets/Scripts/Core/XMLSaver/TypeHolder.cs
+++
@@ -1,6 +0,0 @@
-﻿///Created by Neodrop. neodrop@unity3d.ru
-using UnityEngine;
-
-public class TypeHolder : MonoBehaviour {
-	public PrimitiveType type;
-}

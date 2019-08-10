cff8385ccfeb2c4b59403f5876d0ccaf198fcf9e
Team-on <timonsol13@gmail.com>
2019-08-11 02:30:02 +0300

    Simple boss attack

---
+++ Flixter/Assets/Scripts/Game/Boss/ComponentEnabler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentEnabler : MonoBehaviour {
+	public Behaviour component;
+
+	public void Enable() {
+		component.enabled = true;
+	}
+
+	public void Disable() {
+		component.enabled = false;
+	}
+}

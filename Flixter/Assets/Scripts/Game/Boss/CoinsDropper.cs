2b92a7a3948aa330ce581b2a23d2cb748b4f2eff
Team-on <timonsol13@gmail.com>
2019-08-19 01:03:16 +0300

    New coins drop system

--- Flixter/Assets/Scripts/Game/Boss/CoinsDropper.cs
+++ Flixter/Assets/Scripts/Game/Boss/CoinsDropper.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CoinsDropper : MonoBehaviour {
 	public GameObject coinPrefab;
-	public int coinsCount;
+	public int coinsCountMin;
+	public int coinsCountMax;
 	public float dropDistance;
 	public float flightSpeed = 4;
 
 	public void Drop() {
+		int coinsCount = Random.Range(coinsCountMin, coinsCountMax + 1);
 		for (ushort i = 0; i < coinsCount; ++i) {
 			GameObject go = Instantiate(coinPrefab, this.transform.position, Quaternion.identity, this.transform.parent);
 			LinerarMover lm = go.AddComponent<LinerarMover>();

c3375d479dd8e526d3f6af92d48ad2fb32c87676
Team-on <timonsol13@gmail.com>
2019-08-28 23:16:44 +0300

    Boss 3 done

--- Flixter/Assets/Scripts/Game/Boss/Cargo.cs
+++ Flixter/Assets/Scripts/Game/Boss/Cargo.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cargo : BossBase {
-	public GameObject attack;
+	public GameObject reload;
 
 	protected override void ProcessAttack() {
-		LeanTween.cancel(gameObject);
-		EventManager.OnTimeStopChangedEvent -= OnTimeStopChanged;
-		attack.SetActive(true);
+		reload.SetActive(true);
 	}
 }

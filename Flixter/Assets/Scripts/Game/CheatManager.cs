6e8ecca37cc831a8c28ce7579d31a03e26831049
Team-on <timonsol13@gmail.com>
2019-09-02 10:53:39 +0300

    Remove CheatManager

--- Flixter/Assets/Scripts/Game/CheatManager.cs
+++ Flixter/Assets/Scripts/Game/CheatManager.cs
@@ -6,28 +6,28 @@ public class CheatManager : MonoBehaviour {
 	// guarantee this will be always a singleton only - can't use the constructor!
 	protected CheatManager() { }
 
-	public bool CheatsOn = true;
+	//public bool CheatsOn = true;
 
-	public bool PlayerIgnoreDamage = false;
+	//public bool PlayerIgnoreDamage = false;
 
-	void Update() {
-		if (!CheatsOn)
-			return;
+	//void Update() {
+	//	if (!CheatsOn)
+	//		return;
 
-		if (Input.GetKeyDown(KeyCode.P))
-			GameManager.Instance.IsTimeStop = !GameManager.Instance.IsTimeStop;
+	//	if (Input.GetKeyDown(KeyCode.P))
+	//		GameManager.Instance.IsTimeStop = !GameManager.Instance.IsTimeStop;
 
-		if (Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.KeypadPlus)) {
-			var player = GameManager.Instance.Player;
-			++player.currentBulletStartPosUse;
-			if (player.bulletStartPos.Length == player.currentBulletStartPosUse)
-				--player.currentBulletStartPosUse;
-		}
+	//	if (Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.KeypadPlus)) {
+	//		var player = GameManager.Instance.Player;
+	//		++player.currentBulletStartPosUse;
+	//		if (player.bulletStartPos.Length == player.currentBulletStartPosUse)
+	//			--player.currentBulletStartPosUse;
+	//	}
 
-		if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus)) {
-			var player = GameManager.Instance.Player;
-			if (player.currentBulletStartPosUse != 0)
-				--player.currentBulletStartPosUse;
-		}
-	}
+	//	if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus)) {
+	//		var player = GameManager.Instance.Player;
+	//		if (player.currentBulletStartPosUse != 0)
+	//			--player.currentBulletStartPosUse;
+	//	}
+	//}
 }
